099edc4ba379ba135cdea6d906e4da69abc7709f
Lakeshwar <110439492+lakeshwarmandal@users.noreply.github.com>
2023-12-17 15:37:16 -0600

    Task5

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const names = ["Jay", "Sabita", "Ranuk", "Sumitra"];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="side-display">
+        <h2 className="singer">Choose Singer</h2>
+        <ul className="list">
+          {names.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="container">
+        <div className="test">
+          <img
+            className="imge"
+            src="/public/img2.jpeg"
+            alt="img"
+            height={200}
+            width={350}
+          />
+        </div>
+        <h1>Order Summary</h1>
+        <p className="para">
+          To help consumers compare the different features of health benefits
+          and coverage, the Affordable Care Act generally requires all group
+          health plans and health insurance companies to provide individuals a
+          “summary of benefits and coverage” that “accurately describes the
+          benefits and coverage under the plan.”
         </p>
+        <div className="btn-container">
+          <div className="btn-container1">
+            <p id="annual">Annual Plan</p>
+            <p id="price">$33.33/Yers</p>
+          </div>
+          <div id="change">
+            <p>Change</p>
+          </div>
+        </div>
+        <div className="payment">Proceed To Payment</div>
+        <p id="Cancel">Cancell Order</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
